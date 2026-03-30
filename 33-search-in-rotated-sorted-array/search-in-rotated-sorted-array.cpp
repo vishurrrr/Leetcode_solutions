452206422b6e55f6245f562e4154dc4900ab1bf2
Vaishalirwt <vaishalirawattt@gmail.com>
2026-03-30 14:31:45 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (34.83%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,12 +1,33 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==target){
-                 return i;
-            }
+       int index = min_element(nums.begin(), nums.end()) - nums.begin();//min index
+       int low1=0,high1=index-1;
+       while(low1<=high1){
+        int mid=(low1+high1)/2;
+          if(nums[mid]==target){
+            return mid;
         }
-        return -1;
-
+        if(nums[mid]>target){
+            high1=mid-1;
+        }
+        else{
+            low1=mid+1;
+        }
+       }
+       int low2=index,high2=nums.size()-1;
+       while(low2<=high2){
+        int mid=(low2+high2)/2;
+        if(nums[mid]==target){
+            return mid;
+        }
+        if(nums[mid]>target){
+              high2=mid-1;
+        }
+        else{
+             low2=mid+1;
+        }
+       }
+    return -1;
     }
 };
