54faa3f003ce0e012bb1642a3248702ea83f30c8
Vaishalirwt <vaishalirawattt@gmail.com>
2026-04-17 00:49:46 +0530

    Time: 11 ms (60.67%) | Memory: 14.1 MB (33.25%) - LeetSync

--- 16-3sum-closest/3sum-closest.cpp
+++ 16-3sum-closest/3sum-closest.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        int n = nums.size();
         sort(nums.begin(), nums.end());
-
-        int ans = nums[0] + nums[1] + nums[2];
-        int bestDiff = target - ans;
-
-        for (int i = 0; i < n - 2; i++) {
-            int j = i + 1;
-            int k = n - 1;
-
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                int diff = target - sum;
-
-                // ✅ closest check using diff
-                if (abs(diff) < abs(bestDiff)) {
-                    bestDiff = diff;
-                    ans = sum;
+        int n = nums.size();
+        
+        int closestSum = nums[0] + nums[1] + nums[2];
+        
+        for(int i = 0; i < n - 2; i++) {
+            int left = i + 1;
+            int right = n - 1;
+            
+            while(left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                
+                // update closest sum
+                if(abs(target - sum) < abs(target - closestSum)) {
+                    closestSum = sum;
                 }
-
-                if (diff == 0) {
+                
+                if(sum < target) {
+                    left++;
+                }
+                else if(sum > target) {
+                    right--;
+                }
+                else {
                     return sum; // exact match
-                } else if (diff > 0) {
-                    j++;   // sum too small
-                } else {
-                    k--;   // sum too large
                 }
             }
         }
-        return ans;
+        
+        return closestSum;
     }
 };
