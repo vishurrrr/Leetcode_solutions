aa4136a9d5cb5f2f45e9b172a99c6160c136d754
Vaishalirwt <vaishalirawattt@gmail.com>
2026-07-09 14:11:27 +0530

    Time: 0 ms (100.00%) | Memory: 28 MB (5.06%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -5,14 +5,12 @@ public:
         for(int i=0;i<s.size();i++){
             st.push(s[i]);
         }
-        string res="";
-        while(!st.empty()){
-            char c=st.top();
-            res+=c;
+        
+        for(int i=0;i<s.size();i++){
+            s[i]=st.top();
             st.pop();
+
         }
-    for(int i=0;i<s.size();i++){
-        s[i]=res[i];
-    }
+  
     }
 };
